53680d1c83685da8d6f22756f79f9efe5cb1110e
mark <mark@rgames.jp>
2020-06-04 17:57:07 +0900

    deploy commit

--- deploy/a226bb6ffff6d14acc2d3d928acd511450dd6a48/main.f3405.js
+++ deploy/a226bb6ffff6d14acc2d3d928acd511450dd6a48/main.f3405.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.3668f.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.43556.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
